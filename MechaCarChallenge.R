fa81768f60556ea5e2c4cd8b4ab5781813e0ed6f
I-CV207 <83261520+I-CV207@users.noreply.github.com>
2021-09-24 21:31:34 -0500

    Challenge 15

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,10 +1,30 @@
-#Deliverable 1
-
-# 3. Use the library() function to load the dplyr package.
+# Use the library() function to load the dplyr package.
 library(dplyr)
-# 4. Import and read the MechaCar_mpg.csv dile as a dataframe.
+
+# LINEAR REGRESSION MECHACAR PROTOTYPE
+
+# Import and read the MechaCar_mpg.csv file as a dataframe.
 mecha_car_data <- read.csv("Resources/MechaCar_mpg.csv")
-# 5. Perform linear regression using lm() function.
+# Perform linear regression using lm() function.
 lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_car_data)
-# 6.Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+# Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_car_data))
+
+# SUMMARY STATISTICS OF SUSPENSION COILS
+
+# Import and read the Suspension_coil.csv file as a dataframe.
+suspension_coil_data <- read.csv("Resources/Suspension_coil.csv")
+# Create a total_summary dataframe
+total_summary <- suspension_coil_data %>% summarise(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Standard_deviation=sd(PSI),.groups = "keep")
+# Create a lot_summary dataframe
+lot_summary<- suspension_coil_data %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),Standard_deviation=sd(PSI),.groups = "keep")
+
+# T-TEST ON SUSPENSION COILS
+# Determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(suspension_coil_data$PSI,mu=1500)
+# Determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+
+t.test(subset(suspension_coil_data,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+t.test(subset(suspension_coil_data,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+t.test(subset(suspension_coil_data,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
+
